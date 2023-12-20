e853a54623122df7194f1c342f53af947308bcbc
kent riggs <k2r3@proton.me>
2023-12-20 08:48:29 -0700

    added code to solve for tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"];
+
+function writeCards(cards, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < cards.length; i++) {
+    thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  console.log(thankYouMessages); 
+  return thankYouMessages;
+}
+
+writeCards(cards, "birthday");
+
+function countDown(startingNumber) {
+    
+for (let i = startingNumber; i >= 0; i--) {
+  console.log(i);
+}
+}
